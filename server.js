7e3ed74affb2d1b8d90729a6d9fee47399503d8b
norulshahlam <norulshahlam@gmail.com>
2021-03-30 10:45:17 +0800

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const path = "path";
+const path = require("path");
 const app = express();
 
 //connect to DB
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) =>
-    res.sendFiile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
 
